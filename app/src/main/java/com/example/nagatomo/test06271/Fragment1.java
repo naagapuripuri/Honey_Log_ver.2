fb03c667d1e45e1bda28e4f303802e28d71a0a6e
s1520217 <nagatomo1218@gmail.com>
2015-06-28 10:39:31 +0900

    second

--- app/src/main/java/com/example/nagatomo/test06271/Fragment1.java
+++ app/src/main/java/com/example/nagatomo/test06271/Fragment1.java
@@ -5,15 +5,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Nagatomo on 2015/06/28.
  */
 class Fragment1 extends Fragment {
+    TextView text;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab1_fragment, container, false);
+        View v = inflater.inflate(R.layout.tab1_fragment, container, false);
+
+        text = (TextView) v.findViewById(R.id.tab1text);
+        text.setText("TAB1の内容（fragment）");
+
+
+        return v;
     }
 }
 
